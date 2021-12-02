40fa4f8eb6296a897f4b1f69b5b3282cdc844adf
chunu-j <codehouse9@naver.com>
2021-12-03 00:17:06 +0900

    first commit

---
+++ src/main/java/housepig/simpartyapi/SimpartyApiApplication.java
@@ -0,0 +1,13 @@
+package housepig.simpartyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpartyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpartyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/housepig/simpartyapi/SimpartyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package housepig.simpartyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpartyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
